97c0fb99f71c651d46964c80a3bce1801ee4dd80
boksil9 <94672332+boksil9@users.noreply.github.com>
2025-11-05 19:45:07 +0900

    JobTimer 수정
    - System Timer 사용하도록 변경
    - delay 이후 원래 JobQueue에 다시 던짐

---
+++ Job/JobTimerManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+
+namespace ServerLib
+{
+	class JobTimer : System.Timers.Timer
+	{
+		public JobTimer(double interval, JobBase job)
+			: base(interval)
+		{
+			Job = job;
+		}
+
+        public JobBase Job { get; set; }
+	}
+
+	public class JobTimerManager
+	{
+        public void Push(JobBase job, int delay = 0)
+		{
+			JobTimer timer = new JobTimer(delay, job);
+	
+			timer.Elapsed += TimerCallBack;
+            timer.AutoReset = false;
+			timer.Start();
+		}
+
+		static void TimerCallBack(object sender, ElapsedEventArgs e)
+		{
+			JobTimer timer = sender as JobTimer;
+			if (timer == null)
+				return;
+
+			JobQueue queue = timer.Job.Queue;
+			if (queue == null)
+				return;
+
+			queue.Push(timer.Job);
+		}
+	}
+}
